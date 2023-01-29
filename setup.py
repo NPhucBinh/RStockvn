b2a2605fef3251e57f7281eea791de4080c0dad2
NPhucBinh <nguyenphucbinh67@gmail.com>
2023-01-29 12:14:11 +0700

    fix file setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,25 @@ import codecs
 hs=os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(hs,'README.md'),encoding='utf-8') as fh:
-    long_description = "\n" + fh.read()
+    long_description = fh.read()
 
 DS = 'Report Finance of Companies in Vietnamese'
 
 #Setting
 setup(
     name='RStockvn',
-    version='0.1.0',
+    version='0.1.2',
     author='NGUYEN PHUC BINH',
     author_email='nguyenphucbinh67@gmail.com',
     description=DS,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
+    url="https://github.com/NPhucBinh/RStockvn",
     install_requires=['pandas','requests','json'],
     keywords=['RStockvn','rstockvn','report stock vn'],
-    url='https://github.com/NPhucBinh/RStockvn',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",]
 )
